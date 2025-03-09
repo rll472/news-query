e3ace3b7c11e7b45603484eb4157fe9305344d5f
Russ <your-email@example.com>
2025-03-09 18:44:58 -0500

    Forced dynamic rendering

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,4 +1,8 @@
 // src/app/not-found.jsx
+"use client";
+
+export const dynamic = "force-dynamic";
+
 export default function NotFound() {
-    return <div>404 - Page Not Found</div>;
-  }
+  return <div>404 - Page Not Found. Check URL: {window.location.href}</div>;
+}
