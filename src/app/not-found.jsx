06d0582bba5d9c68a79ada911889a07b2da8fca3
Russ <your-email@example.com>
2025-03-09 18:03:57 -0500

    Fix window undefined error in not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,3 +1,6 @@
+// src/app/not-found.jsx
+"use client";
+
 export default function NotFound() {
-    return <div>404 - Page Not Found. Check URL: {window.location.href}</div>;
-  }
+  return <div>404 - Page Not Found. Check URL: {window.location.href}</div>;
+}
